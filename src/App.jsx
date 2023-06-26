7450058454dec4c693a0b740bca44d72e9d88400
rodolfodom <rodolfodom7@gmail.com>
2023-06-25 19:34:29 -0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y:0})
+
+  useEffect(() => {
+    console.log(`Enabled: ${enabled}`)
+    const handleMove = (event)=>{
+      const{clientX, clientY} = event
+      console.log('habdlemove: ', {clientX, clientY})
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if(enabled) window.addEventListener('pointermove', handleMove)
+
+    return ()=>{
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({x: 0, y: 0})
+    }
+  }, [enabled])
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{
+        position: 'absolute',
+        background: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+      <button onClick={()=>setEnabled(!enabled)}>{enabled? "Dejar de seguir puntero": "Seguir puntero"}</button>
     </>
   )
 }
